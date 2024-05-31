d6ab8e41a71dba6b65bd7bb70c4bb162b4aa282e
Daniel <daniel.ligory@gmail.com>
2024-05-31 23:46:03 +0100

    Refactor: Quiz Compeleted
    Understanding
    -- Type and Type Conversion

---
+++ Udacity/Introduction to Python/Types and Type Conversion/Question5.py
@@ -0,0 +1,11 @@
+mon_sales = 121
+tues_sales = 105
+wed_sales = 110
+thurs_sales = 98
+fri_sales = 95
+
+#TODO: Print a string with this format: This week's total sales: xxx
+# You will probably need to write some lines of code before the print statement.
+total_sales = int(mon_sales) + int(tues_sales) + int(wed_sales) + int(thurs_sales) + int(fri_sales)
+total_sales_str = str(total_sales)
+print ("This week's total sales: " + total_sales_str)

15d8335db86234908c66312c759198b652c39477
Daniel <daniel.ligory@gmail.com>
2024-06-02 22:15:06 +0100

    Quiz Completed
    Understanding
    -- String Methods

---
+++ Udacity/Introduction to Python/String Methods/quiz.py
@@ -0,0 +1,8 @@
+# Write two lines of code below, each assigning a value to a variable
+name = "Luna"
+pet = "cat"
+
+# Now write a print statement using .format() to print out a sentence and the 
+# values of both of the variables
+
+print ("{} has a pet {}".format(name, pet))

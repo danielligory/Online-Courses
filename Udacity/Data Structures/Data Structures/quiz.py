3e0564ab614ffa82b1cb0d5795018de66b32daea
Daniel <daniel.ligory@gmail.com>
2024-06-13 17:58:35 +0100

    Data Structures - Quiz Completed
    Understanding
    -- Data Structures

---
+++ Udacity/Data Structures/Data Structures/quiz.py
@@ -0,0 +1,7 @@
+# invalid dictionary - this should break
+room_numbers = {
+    ('Freddie', 'Jen'): 403,
+    ('Ned', 'Keith'): 391,
+    ('Kristin', 'Jazzmyne'): 411,
+    ('Eugene', 'Zach'): 395
+}

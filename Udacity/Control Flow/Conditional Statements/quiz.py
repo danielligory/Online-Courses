37e25fed53396a25007a6a036f9ed8494657d1c5
Daniel <daniel.ligory@gmail.com>
2024-06-17 00:06:43 +0100

    Conditional Statement - Quiz Completed
    Understanding
    -- Conditional Statements

---
+++ Udacity/Control Flow/Conditional Statements/quiz.py
@@ -0,0 +1,13 @@
+points = 200  # use this input to make your submission
+
+# write your if statement here
+if points <= 50:
+    result = "Congratulations! You won a wooden rabbit!"
+elif points <= 150:
+    result = "Oh dear, no prize this time."
+elif points <= 180:
+    result = "Congratulations! You won a wafer-thin mint!"
+else:
+    result = "Congratulations! You won a penguin!"
+    
+print(result)

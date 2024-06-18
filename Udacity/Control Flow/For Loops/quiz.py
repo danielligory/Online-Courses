57d745499632d5d0568795dd80045af82d42821d
Daniel <daniel.ligory@gmail.com>
2024-06-18 20:43:19 +0100

    For Loops - Quiz Completed
    Understanding
    -- For Loops

---
+++ Udacity/Control Flow/For Loops/quiz.py
@@ -0,0 +1,8 @@
+names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+usernames = []
+
+# write your for loop here
+for name in names:
+    usernames.append(name.lower().replace(" ", "_"))
+
+print(usernames)
---
+++ Udacity/Control Flow/For Loops/quiz2.py
@@ -0,0 +1,7 @@
+usernames = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+
+# write your for loop here
+for i in range(len(usernames)):
+    usernames[i] = usernames[i].lower().replace(" ", "_")
+
+print(usernames)
